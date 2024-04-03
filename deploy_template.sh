5e39b57951fb93e19e45a304b762e6e33a05c47a
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2024-04-03 10:13:29 +0700

    fix deploy template

--- deploy_template.sh
+++ deploy_template.sh
@@ -2,14 +2,31 @@ csv_path="[path to csv]"
 merkle_tree_path="[path to folder that stores merkle tree proof]"
 token_decimals="[Token decimals]"
 token_mint="[Token mint address]"
-keypair_path="[Path to keypair]"
-rpc="http://127.0.0.1:8899"
-clawback_start_ts="[Clawback date, should be in future]"
-enable_slot="[Enable slot]"
-base_path="[Path to base key]"
-clawback_receiver_owner="[Clawback receiver owner]"
 
-## caculated variable
+# can genrate a random keypair to sign
+base_path="[Path to base key]"  
+
+# Keypair_path to the address that will deploy distributor (admin), that address also need to prepare enough token to fund merkle tree
+# Note: after deployment is suscessful, and team doesn't need to update anything, admin of distributor should be transfer  to team's multisig
+# Command: 
+# target/debug/cli --mint $token_mint --base $base_key --keypair-path $keypair_path --rpc-url $rpc set-admin --new-admin $new_admin --merkle-tree-path $merkle_tree_path
+keypair_path="[Path to keypair]"
+
+rpc="http://127.0.0.1:8899"
+# clawback_start_ts should be in future, at least 1 day from current time
+clawback_start_ts="[Clawback date]"
+
+# refer command to get enable slot, user is able to claim after enable_slot
+# target/debug/cli --rpc-url $rpc slot-by-time --timestamp $time_to_enable_claiming
+# Note: because real timestamp may be diff from slot timestamp, so team may want to adjust enable slot in the time that closes to the launch time, so admin can adjust enable slot by the command
+# target/debug/cli --mint $token_mint --base $base_key --priority-fee $priority_fee --keypair-path $keypair_path --rpc-url $rpc set-enable-slot --merkle-tree-path $merkle_tree_path --slot $enable_slot
+enable_slot="[Enable slot]"  
+
+# the address that will receive token that user haven't claimed yet, should be team's multisig
+clawback_receiver_owner="[Clawback receiver owner]"  
+
+
+## caculated variable, can ignore this
 # kv_path="[path to kv proofs]"
 priority_fee=1000000 # priority fee, can use other number
 max_nodes_per_tree=12000 # default value, can ignore the field
@@ -21,24 +38,11 @@ admin=$(solana-keygen pubkey $keypair_path)
 echo "create merkle tree proof"
 target/debug/cli create-merkle-tree --csv-path $csv_path --merkle-tree-path $merkle_tree_path --max-nodes-per-tree $max_nodes_per_tree --amount 0 --decimals $token_decimals
 
-# echo "generate kv proof"
-# target/debug/cli --mint $token_mint --base $base_key generate-kv-proof --merkle-tree-path $merkle_tree_path --kv-path $kv_path --max-entries-per-file 100000
-
 echo "deploy distributor"
 target/debug/cli --mint $token_mint --priority-fee $priority_fee --keypair-path $keypair_path --rpc-url $rpc new-distributor --start-vesting-ts $start_vesting_ts --end-vesting-ts $end_vesting_ts --merkle-tree-path $merkle_tree_path --base-path $base_path --clawback-start-ts $clawback_start_ts --enable-slot $enable_slot --clawback-receiver-owner $clawback_receiver_owner --closable
-
 
 echo "fund distributor"
 target/debug/cli --mint $token_mint --priority-fee $priority_fee --base $base_key --keypair-path $keypair_path --rpc-url $rpc fund-all --merkle-tree-path $merkle_tree_path
 
-# verify
 echo "verify"
 target/debug/cli --mint $token_mint --base $base_key --rpc-url $rpc verify --merkle-tree-path $merkle_tree_path --clawback-start-ts $clawback_start_ts --enable-slot  $enable_slot --admin $admin --clawback-receiver-owner $clawback_receiver_owner --closable
-
-
-# transfer admin to multisig"
-
-# adjust enable slot
-# echo "adjust enable slot"
-# adjust_slot="[Adjust slot]"
-# target/debug/cli --mint $token_mint --base $base_key --priority-fee $priority_fee --keypair-path $keypair_path --rpc-url $rpc set-enable-slot --merkle-tree-path $merkle_tree_path --slot $adjust_slot
