a5a46b4887469e8bb9917da112b2028e9591ef07
andrewsource147 <31321699+andrewsource147@users.noreply.github.com>
2024-07-01 08:46:50 +0700

    support decimals (#9)

--- merkle-tree/src/tree_node.rs
+++ merkle-tree/src/tree_node.rs
@@ -1,10 +1,10 @@
 use std::str::FromStr;
 
+use crate::csv_entry::CsvEntry;
+use rust_decimal::{prelude::FromPrimitive, Decimal};
 use serde::{Deserialize, Serialize};
 use solana_program::{hash::hashv, pubkey::Pubkey};
 use solana_sdk::hash::Hash;
-
-use crate::csv_entry::CsvEntry;
 
 /// Represents the claim information for an account.
 #[derive(Debug, Clone, Eq, Hash, PartialEq, Serialize, Deserialize)]
@@ -33,17 +33,56 @@ impl TreeNode {
 }
 
 /// Converts a ui amount to a token amount (with decimals)
-fn ui_amount_to_token_amount(amount: u64, decimals: u32) -> u64 {
-    amount * 10u64.checked_pow(decimals).unwrap()
+pub fn ui_amount_to_token_amount(amount: &str, decimals: u32) -> u64 {
+    let amount = Decimal::from_str(amount).unwrap();
+    let amount = amount
+        .checked_mul(Decimal::from_u64(10u64.checked_pow(decimals).unwrap()).unwrap())
+        .unwrap();
+    let amount = amount.floor();
+    amount.try_into().unwrap()
 }
 
 impl TreeNode {
     pub fn from_csv(entry: CsvEntry, decimals: u32) -> Self {
         let node = Self {
             claimant: Pubkey::from_str(entry.pubkey.as_str()).unwrap(),
-            amount: ui_amount_to_token_amount(entry.amount, decimals),
+            amount: ui_amount_to_token_amount(entry.amount.as_str(), decimals),
             proof: None,
         };
         node
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+    #[test]
+    fn test_ui_amount_to_token_amount() {
+        let amount = "3.1234";
+        assert_eq!(ui_amount_to_token_amount(amount, 3), 3123);
+
+        let amount = "0.1234";
+        assert_eq!(ui_amount_to_token_amount(amount, 2), 12);
+
+        let amount = "0.00001";
+        assert_eq!(ui_amount_to_token_amount(amount, 2), 0);
+    }
+
+    #[test]
+    fn test_csv_decimals_parsing() {
+        let path = PathBuf::from("./test_fixtures/test_csv_decimal.csv");
+        let entries = CsvEntry::new_from_file(&path).expect("Failed to parse CSV");
+        assert_eq!(entries.len(), 3);
+        let decimals = 6;
+
+        let tree_nodes: Vec<TreeNode> = entries
+            .into_iter()
+            .map(|x| TreeNode::from_csv(x, decimals))
+            .collect();
+
+        assert_eq!(tree_nodes[0].amount, 1000123456);
+        assert_eq!(tree_nodes[1].amount, 2000123456);
+        assert_eq!(tree_nodes[2].amount, 1500123456);
     }
 }
